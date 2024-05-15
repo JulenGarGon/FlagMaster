58545aad18817d47cae2f2b06083ee993f049c9b
JulenGarGon <jgarciago.inf@upsa.es>
2024-05-15 13:16:29 +0200

    Initial Commit

---
+++ FlagMaster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlagMaster
+//
+//  Created by Julen García Gonzalo on 15/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
